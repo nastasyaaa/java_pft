8be36efa1b2c05db565945f7539f877224a252bf
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-02-07 20:35:41 +0400

    Создание классов: Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/firstprog.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/firstprog.java
@@ -1,7 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 public class firstprog {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+
+    public static void main(String[] args) {  //функция, main - имя функции
+		hello("world");  //функция
+		hello("user"); //функция
+
+        Square s = new Square(5);
+		System.out.println("Площадь квадрата " + s.l + "=" + s.area());
+
+		Rectangle r = new Rectangle(4,5);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + "=" + r.area());
 	}
+
+	public static void hello(String somebody) {  //функция, hello - имя функции, string somebody - параметр/аргумент функции
+		System.out.println("Hello," + somebody + "!");  //содержимое функции с переменной
+    }
+
 }
+
