667d15534b00454ecbd25f3634b3a12dbf197ae2
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-02-27 21:58:04 +0400

    Добавлены тесты создания, редактирования и удаления для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -27,4 +27,24 @@ public class ContactHelper extends HelperBase{
     public void newContactCreation() {
         click(By.linkText("add new"));
     }
+
+    public void selectContact() {
+        click(By.name("selected[]"));
+    }
+
+    public void deleteContact() {
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+    }
+
+    public void intContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
+
+    public void endDeleteContact() {
+        wd.switchTo().alert().accept();
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModification.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactDate;
+
+public class ContactModification extends TestBase{
+    @Test
+    public void testContactModification() {
+        app.getContactHelper().selectContact();
+        app.getContactHelper().intContactModification();
+        app.getContactHelper().fillContactCreation(new ContactDate("Ivanov", "Ivan", "Ivancik", "LK", "Russia"));
+        app.getContactHelper().submitContactModification();
+}
+}
