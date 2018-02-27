e7b43c442a7ec08ba539e321f7ff517674e00988
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-02-27 19:40:25 +0400

    Создание теста GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,4 +35,12 @@ public class GroupHelper extends HelperBase {
     public void selectGroup() {
         click(By.name("selected[]"));
     }
+
+    public void intGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupDate;
+
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().intGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
