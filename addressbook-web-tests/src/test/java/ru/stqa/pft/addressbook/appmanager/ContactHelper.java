fae55bd08028b02254afd18b25452391a707ca6e
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-02-27 19:10:41 +0400

    Создание класса BaseHelper. ДЗ№5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,36 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactDate;
 
-public class ContactHelper {
-   private FirefoxDriver wd;
+public class ContactHelper extends HelperBase{
+
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void returnToContactPage() {
-        wd.findElement(By.linkText("home")).click();
+        click(By.linkText("home"));
     }
 
     public void fillContactCreation(ContactDate contactDate) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactDate.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactDate.getLastname());
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(contactDate.getNickname());
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(contactDate.getCompany());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactDate.getAddress());
+        type(By.name("firstname"), contactDate.getFirstname());
+        type(By.name("lastname"), contactDate.getLastname());
+        type(By.name("nickname"), contactDate.getNickname());
+        type(By.name("company"), contactDate.getCompany());
+        type(By.name("address"), contactDate.getAddress());
+
     }
 
     public void newContactCreation() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 }
