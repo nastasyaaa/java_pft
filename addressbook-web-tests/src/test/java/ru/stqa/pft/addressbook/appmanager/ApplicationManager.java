5bb26c46ec2f77520b301694b528dea9e09c9f78
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-03-01 00:05:02 +0400

    Обновление тестов для запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,22 +1,38 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
 
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+       if (browser == org.openqa.selenium.remote.BrowserType.FIREFOX) {
+           wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+       } else if (browser == org.openqa.selenium.remote.BrowserType.CHROME){
+           wd = new ChromeDriver();
+       } else if (browser == org.openqa.selenium.remote.BrowserType.IE) {
+           wd = new InternetExplorerDriver();
+       }
+
        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
        wd.get("http://localhost/addressbook/");
        contactHelper = new ContactHelper(wd);
