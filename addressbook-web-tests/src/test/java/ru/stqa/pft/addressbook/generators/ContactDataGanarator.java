d2ea25ab5ac3dada0750e4f368983e0b867db9b7
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-04-07 17:52:54 +0400

    Генератор контактов (ДЗ 12)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGanarator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGanarator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactDate;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ContactDataGanarator {
 
@@ -29,7 +31,7 @@ public class ContactDataGanarator {
         JCommander jCommander = new JCommander(ganarator);
         try {
             jCommander.parse(args);
-        } catch (ParameterException ex){
+        } catch (ParameterException ex) {
             jCommander.usage();
             return;
         }
@@ -39,22 +41,37 @@ public class ContactDataGanarator {
 
     private void run() throws IOException {
         List<ContactDate> contacts = generateContacts(count);
-        save(contacts, new File(file));
+        if (format.equals("csv")) {
+            saveAsCsv(contacts, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
+        }
+
     }
 
-    private void save(List<ContactDate> contacts, File file) throws IOException {
-        System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (ContactDate contact : contacts) {
-            writer.write(String.format("%s;%s\n", contact.getFirstname(), contact.getLastname()));
-        }
-        writer.close();
+    private void saveAsCsv(List<ContactDate> contacts, File file) throws IOException {
+            System.out.println(new File(".").getAbsolutePath());
+            Writer writer = new FileWriter(file);
+            for (ContactDate contact : contacts) {
+                writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname()));
+            }
+            writer.close();
+    }
 
+    private void saveAsXml(List<ContactDate> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactDate.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
     }
 
     private List<ContactDate> generateContacts(int count) {
         List<ContactDate> contacts = new ArrayList<ContactDate>();
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             contacts.add(new ContactDate().withFirstname(String.format("Ivanov %s", i))
                     .withLastname(String.format("Ivan %s", i)));
         }
