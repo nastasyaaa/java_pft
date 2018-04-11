d9df5fe204a49f18fc36a9dad7be5d694d9a44aa
Anastasiya Smirnova <nastasyaaa@yandex.ru>
2018-04-11 16:07:32 +0400

    Реализована ленивая инициализация помощников (8.4)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import ru.stqa.pft.mantis.tests.RegistrationTests;
 
 import java.io.File;
 import java.io.FileReader;
@@ -15,10 +16,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
-
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -29,23 +30,13 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(org.openqa.selenium.remote.BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
-
-
     }
 
-
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
+
     }
 
     public HttpSession newSession(){
@@ -54,5 +45,29 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(org.openqa.selenium.remote.BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+
+        }
+        return wd;
     }
 }
